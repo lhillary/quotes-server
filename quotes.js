d3330914f78f0ccffef3faaa5c0030ca1f1e4534
Lydia <lydia.hillary@gmail.com>
2019-05-16 21:39:11 -0400

    Done, created server and made it functional.

---
+++ quotes.js
@@ -0,0 +1,11 @@
+const quotes = [
+    "I used to think I was indecisive, but now I'm not too sure.", 
+    "Doing nothing is hard, you never know when you're done.", 
+    "If two wrongs don't make a right, try three.", 
+    "I am not lazy, I am on energy saving mode.", 
+    "A bank is a place that will lend you money if you can prove that you don't need it."
+];
+
+module.exports = {
+    quotes
+};
